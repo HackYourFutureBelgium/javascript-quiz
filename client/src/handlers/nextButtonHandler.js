345e8e3f6118ab2c550e20ddbe52cf8512453cda
fatih <fatihayoz20800@gmail.com>
2021-07-16 14:47:34 +0200

    hints is visible in the second page

--- client/src/handlers/nextButtonHandler.js
+++ client/src/handlers/nextButtonHandler.js
@@ -1,5 +1,7 @@
 import { data } from "../../data/quiz.js";
 import { quizQuestionPanel } from "../components/pages/quiz/quiz-question-panel.js";
+import { createHintPanel } from "../components/pages/quiz/hint-panel.js";
+import { state } from "../init/state.js";
 
 const nextButtonHandler = () => {
   const quizContainer = document.querySelector(".quiz");
@@ -7,6 +9,13 @@ const nextButtonHandler = () => {
   data.indexOfRenderedQuestion++;
   // render the question panel
   quizContainer.appendChild(quizQuestionPanel(data.indexOfRenderedQuestion));
+
+  const hintContainer = document.querySelector(".btn-info");
+  hintContainer.innerHTML = "";
+  state.questions[state.indexOfRenderedQuestion].links++;
+  hintContainer.appendChild(
+    createHintPanel(state.questions[state.indexOfRenderedQuestion].links)
+  );
 };
 
 export { nextButtonHandler };
